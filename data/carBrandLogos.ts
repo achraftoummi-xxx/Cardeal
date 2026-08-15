270a3837c994cbc0eeb1a46b9e96a3f7cf7b8a86
achraftoummi-xxx <achraftoummi@gmail.com>
2026-08-15 23:32:59 +0100

    feat: expand car brand logo mapping with newly added brand assets

--- data/carBrandLogos.ts
+++ data/carBrandLogos.ts
@@ -1,55 +1,121 @@
 import type { StaticImageData } from "next/image";
 import abarthLogo from "@/assets/car-brands/abarth.png";
 import acuraLogo from "@/assets/car-brands/ACURA.png";
+import aixamLogo from "@/assets/car-brands/Aixam.png";
 import alfaRomeoLogo from "@/assets/car-brands/Alpha-romeo.png";
+import alpinaLogo from "@/assets/car-brands/Alpina.png";
+import alpineLogo from "@/assets/car-brands/Alpine.png";
+import apolloLogo from "@/assets/car-brands/Apollo.png";
+import arrineraLogo from "@/assets/car-brands/Arrinera.png";
 import astonMartinLogo from "@/assets/car-brands/ASTON-MARTIN.png";
 import audiLogo from "@/assets/car-brands/audi.png";
+import automobiliPininfarinaLogo from "@/assets/car-brands/Automobili-Pininfarina.png";
+import belazLogo from "@/assets/car-brands/BELAZ.png";
+import bentleyLogo from "@/assets/car-brands/Bentley.png";
 import bmwLogo from "@/assets/car-brands/bmw.png";
+import borgwardLogo from "@/assets/car-brands/Borgward.png";
 import buickLogo from "@/assets/car-brands/BUICK.png";
 import bydLogo from "@/assets/car-brands/BYD.png";
 import cadillacLogo from "@/assets/car-brands/CADILLAC.png";
+import caterhamLogo from "@/assets/car-brands/Caterham.png";
 import cheryLogo from "@/assets/car-brands/Chery.png";
 import chevroletLogo from "@/assets/car-brands/Chevrolet.png";
 import chryslerLogo from "@/assets/car-brands/chrysler.png";
 import citroenLogo from "@/assets/car-brands/citroen.png";
 import cupraLogo from "@/assets/car-brands/cupra.png";
+import daciaLogo from "@/assets/car-brands/Dacia.png";
+import daihatsuLogo from "@/assets/car-brands/Daihatsu.png";
+import datsunLogo from "@/assets/car-brands/Datsun.png";
+import deTomasoLogo from "@/assets/car-brands/De-Tomaso.png";
 import dfskLogo from "@/assets/car-brands/DFSK.png";
+import drAutomobilesLogo from "@/assets/car-brands/DR-Automobiles.png";
 import dsLogo from "@/assets/car-brands/DS.png";
 import ferrariLogo from "@/assets/car-brands/Ferrari.png";
 import fiatLogo from "@/assets/car-brands/fiat.png";
 import fordLogo from "@/assets/car-brands/ford.png";
+import gazLogo from "@/assets/car-brands/GAZ.png";
 import geelyLogo from "@/assets/car-brands/Geely.png";
+import ginettaLogo from "@/assets/car-brands/Ginetta.png";
 import gmcLogo from "@/assets/car-brands/GMC.png";
 import greatWallLogo from "@/assets/car-brands/Greatwall.png";
+import gumpertLogo from "@/assets/car-brands/Gumpert.png";
+import hispanoSuizaLogo from "@/assets/car-brands/Hispano-Suiza.png";
 import hondaLogo from "@/assets/car-brands/honda.png";
 import hyundaiLogo from "@/assets/car-brands/hyundai.png";
 import infinitiLogo from "@/assets/car-brands/INFINITY.png";
+import isderaLogo from "@/assets/car-brands/Isdera.png";
 import isuzuLogo from "@/assets/car-brands/isuzu.png";
+import italdesignLogo from "@/assets/car-brands/Italdesign.png";
+import ivecoLogo from "@/assets/car-brands/Iveco.png";
 import jacLogo from "@/assets/car-brands/JAC.png";
+import jaguarLogo from "@/assets/car-brands/Jaguar.png";
+import kamazLogo from "@/assets/car-brands/KAMAZ.png";
 import kiaLogo from "@/assets/car-brands/kia.png";
+import koenigseggLogo from "@/assets/car-brands/Koenigsegg.png";
+import ktmLogo from "@/assets/car-brands/KTM.png";
+import ladaLogo from "@/assets/car-brands/Lada.png";
+import lagondaLogo from "@/assets/car-brands/Lagonda.png";
+import lamborghiniLogo from "@/assets/car-brands/Lamborghini.png";
 import lanciaLogo from "@/assets/car-brands/Lancia.png";
 import landRoverLogo from "@/assets/car-brands/landroover.png";
+import lastunLogo from "@/assets/car-brands/Lastun.png";
 import lexusLogo from "@/assets/car-brands/Lexus.png";
+import ligierLogo from "@/assets/car-brands/Ligier.png";
+import lotusLogo from "@/assets/car-brands/Lotus.png";
 import mahindraLogo from "@/assets/car-brands/mahindra.png";
 import manLogo from "@/assets/car-brands/MAN.png";
+import maseratiLogo from "@/assets/car-brands/Maserati.png";
+import mazLogo from "@/assets/car-brands/MAZ.png";
 import mazdaLogo from "@/assets/car-brands/mazda.png";
+import mazzantiLogo from "@/assets/car-brands/Mazzanti.png";
+import mclarenLogo from "@/assets/car-brands/McLaren.png";
+import mcmurtryLogo from "@/assets/car-brands/mcmurtry.png";
 import mercedesLogo from "@/assets/car-brands/mercedes.png";
 import mgLogo from "@/assets/car-brands/MG.png";
+import microcarLogo from "@/assets/car-brands/Microcar.png";
 import miniLogo from "@/assets/car-brands/mini-cooper.png";
 import mitsubishiLogo from "@/assets/car-brands/Mitsubitchi.png";
+import mitsuokaLogo from "@/assets/car-brands/Mitsuoka.png";
+import moskvitchLogo from "@/assets/car-brands/Moskvitch.png";
+import mybachLogo from "@/assets/car-brands/Mybach.png";
 import nissanLogo from "@/assets/car-brands/Nissan.png";
+import nobleLogo from "@/assets/car-brands/Noble.png";
 import opelLogo from "@/assets/car-brands/Opel.png";
+import paganiLogo from "@/assets/car-brands/Pagani.png";
 import peugeotLogo from "@/assets/car-brands/peugeot.png";
+import pininfarinaLogo from "@/assets/car-brands/Pininfarina.png";
+import polestarLogo from "@/assets/car-brands/Polestar.png";
 import porscheLogo from "@/assets/car-brands/porshe.png";
+import pragaLogo from "@/assets/car-brands/Praga.png";
+import prodriveLogo from "@/assets/car-brands/Prodrive.svg";
+import protonLogo from "@/assets/car-brands/Proton.png";
 import renaultLogo from "@/assets/car-brands/renault.png";
+import rimacLogo from "@/assets/car-brands/Rimac.png";
 import rollsRoyceLogo from "@/assets/car-brands/RollsRoys.png";
+import roverLogo from "@/assets/car-brands/Rover.png";
+import rufLogo from "@/assets/car-brands/RUF.png";
+import saabLogo from "@/assets/car-brands/Saab.png";
+import scaniaLogo from "@/assets/car-brands/Scania.png";
+import scionLogo from "@/assets/car-brands/Scion.png";
+import seatLogo from "@/assets/car-brands/Seat.png";
+import skodaLogo from "@/assets/car-brands/Škoda.png";
+import smartLogo from "@/assets/car-brands/Smart.png";
+import solarisLogo from "@/assets/car-brands/Solaris.png";
+import spykerLogo from "@/assets/car-brands/Spyker.png";
 import subaruLogo from "@/assets/car-brands/Subaro.png";
 import suzukiLogo from "@/assets/car-brands/SUZUKI.png";
 import tataLogo from "@/assets/car-brands/TATA.png";
+import tatraLogo from "@/assets/car-brands/Tatra.png";
+import tauroLogo from "@/assets/car-brands/Tauro.png";
 import teslaLogo from "@/assets/car-brands/tESLA.png";
 import toyotaLogo from "@/assets/car-brands/toyota.png";
+import tvrLogo from "@/assets/car-brands/TVR.png";
+import uazLogo from "@/assets/car-brands/UAZ.png";
+import vauxhallLogo from "@/assets/car-brands/Vauxhall.png";
+import venturiLogo from "@/assets/car-brands/Venturi.png";
 import volvoLogo from "@/assets/car-brands/volvo.png";
 import volkswagenLogo from "@/assets/car-brands/VW.png";
+import wiesmannLogo from "@/assets/car-brands/Wiesmann.png";
 import defaultCarBrandLogo from "@/assets/dealers_logos/default.png";
 
 /* File registry — keyed by the normalized filename stem of every asset
@@ -60,55 +126,121 @@ import defaultCarBrandLogo from "@/assets/dealers_logos/default.png";
 const CAR_BRAND_FILE_REGISTRY: Record<string, { src: string }> = {
   abarth: { src: abarthLogo.src },
   acura: { src: acuraLogo.src },
+  aixam: { src: aixamLogo.src },
   alpharomeo: { src: alfaRomeoLogo.src },
+  alpina: { src: alpinaLogo.src },
+  alpine: { src: alpineLogo.src },
+  apollo: { src: apolloLogo.src },
+  arrinera: { src: arrineraLogo.src },
   astonmartin: { src: astonMartinLogo.src },
   audi: { src: audiLogo.src },
+  automobilipininfarina: { src: automobiliPininfarinaLogo.src },
+  belaz: { src: belazLogo.src },
+  bentley: { src: bentleyLogo.src },
   bmw: { src: bmwLogo.src },
+  borgward: { src: borgwardLogo.src },
   buick: { src: buickLogo.src },
   byd: { src: bydLogo.src },
   cadillac: { src: cadillacLogo.src },
+  caterham: { src: caterhamLogo.src },
   chery: { src: cheryLogo.src },
   chevrolet: { src: chevroletLogo.src },
   chrysler: { src: chryslerLogo.src },
   citroen: { src: citroenLogo.src },
   cupra: { src: cupraLogo.src },
+  dacia: { src: daciaLogo.src },
+  daihatsu: { src: daihatsuLogo.src },
+  datsun: { src: datsunLogo.src },
+  detomaso: { src: deTomasoLogo.src },
   dfsk: { src: dfskLogo.src },
+  drautomobiles: { src: drAutomobilesLogo.src },
   ds: { src: dsLogo.src },
   ferrari: { src: ferrariLogo.src },
   fiat: { src: fiatLogo.src },
   ford: { src: fordLogo.src },
+  gaz: { src: gazLogo.src },
   geely: { src: geelyLogo.src },
+  ginetta: { src: ginettaLogo.src },
   gmc: { src: gmcLogo.src },
   greatwall: { src: greatWallLogo.src },
+  gumpert: { src: gumpertLogo.src },
+  hispanosuiza: { src: hispanoSuizaLogo.src },
   honda: { src: hondaLogo.src },
   hyundai: { src: hyundaiLogo.src },
   infinity: { src: infinitiLogo.src },
+  isdera: { src: isderaLogo.src },
   isuzu: { src: isuzuLogo.src },
+  italdesign: { src: italdesignLogo.src },
+  iveco: { src: ivecoLogo.src },
   jac: { src: jacLogo.src },
+  jaguar: { src: jaguarLogo.src },
+  kamaz: { src: kamazLogo.src },
   kia: { src: kiaLogo.src },
+  koenigsegg: { src: koenigseggLogo.src },
+  ktm: { src: ktmLogo.src },
+  lada: { src: ladaLogo.src },
+  lagonda: { src: lagondaLogo.src },
+  lamborghini: { src: lamborghiniLogo.src },
   lancia: { src: lanciaLogo.src },
   landroover: { src: landRoverLogo.src },
+  lastun: { src: lastunLogo.src },
   lexus: { src: lexusLogo.src },
+  ligier: { src: ligierLogo.src },
+  lotus: { src: lotusLogo.src },
   mahindra: { src: mahindraLogo.src },
   man: { src: manLogo.src },
+  maserati: { src: maseratiLogo.src },
+  maz: { src: mazLogo.src },
   mazda: { src: mazdaLogo.src },
+  mazzanti: { src: mazzantiLogo.src },
+  mclaren: { src: mclarenLogo.src },
+  mcmurtry: { src: mcmurtryLogo.src },
   mercedes: { src: mercedesLogo.src },
   mg: { src: mgLogo.src },
+  microcar: { src: microcarLogo.src },
   minicooper: { src: miniLogo.src },
   mitsubitchi: { src: mitsubishiLogo.src },
+  mitsuoka: { src: mitsuokaLogo.src },
+  moskvitch: { src: moskvitchLogo.src },
+  mybach: { src: mybachLogo.src },
   nissan: { src: nissanLogo.src },
+  noble: { src: nobleLogo.src },
   opel: { src: opelLogo.src },
+  pagani: { src: paganiLogo.src },
   peugeot: { src: peugeotLogo.src },
+  pininfarina: { src: pininfarinaLogo.src },
+  polestar: { src: polestarLogo.src },
   porshe: { src: porscheLogo.src },
+  praga: { src: pragaLogo.src },
+  prodrive: { src: prodriveLogo.src },
+  proton: { src: protonLogo.src },
   renault: { src: renaultLogo.src },
+  rimac: { src: rimacLogo.src },
   rollsroys: { src: rollsRoyceLogo.src },
+  rover: { src: roverLogo.src },
+  ruf: { src: rufLogo.src },
+  saab: { src: saabLogo.src },
+  scania: { src: scaniaLogo.src },
+  scion: { src: scionLogo.src },
+  seat: { src: seatLogo.src },
+  skoda: { src: skodaLogo.src },
+  smart: { src: smartLogo.src },
+  solaris: { src: solarisLogo.src },
+  spyker: { src: spykerLogo.src },
   subaro: { src: subaruLogo.src },
   suzuki: { src: suzukiLogo.src },
   tata: { src: tataLogo.src },
+  tatra: { src: tatraLogo.src },
+  tauro: { src: tauroLogo.src },
   tesla: { src: teslaLogo.src },
   toyota: { src: toyotaLogo.src },
+  tvr: { src: tvrLogo.src },
+  uaz: { src: uazLogo.src },
+  vauxhall: { src: vauxhallLogo.src },
+  venturi: { src: venturiLogo.src },
   volvo: { src: volvoLogo.src },
   vw: { src: volkswagenLogo.src },
+  wiesmann: { src: wiesmannLogo.src },
 };
 
 /* Brand → asset name mapping. Direct matches are resolved automatically by
@@ -117,12 +249,15 @@ const CAR_BRAND_FILE_REGISTRY: Record<string, { src: string }> = {
    diacritics like Citroën). */
 const BRAND_TO_FILE: Record<string, string> = {
   alfaromeo: "alpharomeo",
+  astonmartinlagonda: "lagonda",
   dsautomobiles: "ds",
   greatwallmotors: "greatwall",
   infiniti: "infinity",
   landrover: "landroover",
+  maybach: "mybach",
   mini: "minicooper",
   mitsubishi: "mitsubitchi",
+  moskvich: "moskvitch",
   porsche: "porshe",
   rollsroyce: "rollsroys",
   subaru: "subaro",
